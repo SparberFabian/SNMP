342a7004732807dc9a104df24b2fe1cddba2d5e2
SparberFabian <sparberfabian2002@gmail.com>
2020-12-17 19:35:25 +0100

    third commit

--- README.md
+++ README.md
@@ -6,5 +6,14 @@ Ich werde die Programmiersprache Java verwenden.
 
 #Meilenstein 2
 
-Ich habe mich für die Java Bibliothek snmp4j entschieden und habe begonnen die Mindestanforderungen zu bearbeiten. Bisher habe ich es geschafft ein snmpget zu schreiben. Es können verschieden Informationen mit Hilfe einer OID abgefragt werden. Noch in Arbeit ist ein Terminal mit dem man die OIDs und IP Adressen selbst eingeben kann bzw. nicht im Code eingeben muss. 
+Ich habe mich für die Java Bibliothek snmp4j entschieden und habe begonnen die Mindestanforderungen zu bearbeiten. Bisher habe ich 
+es geschafft ein snmpget zu schreiben. Es können verschieden Informationen mit Hilfe einer OID abgefragt werden. Noch in Arbeit ist 
+ein Terminal mit dem man die OIDs und IP Adressen selbst eingeben kann bzw. nicht im Code eingeben muss. 
 
+#Meilenstein 3
+
+Für den 3. Meilenstein habe ich das Programm erweitert und ein getNetwork zum SNMP Tool hinzugefügt. Des weiteren können jetzt auch 
+IP-Adressen als Benutzer eingegeben werden und müssen nicht im Code ausgetauscht werden. Bei der getNetwork Operation werden derzeit 
+nur Netzwerke der Klasse C verarebitet werden woran aber noch gearbeitet wird. In der getNetwork Methode wird auf jede erreichbare 
+IP ein SNMPget ausgeführt, dass bei Geräten die kein SNMP aktiviert haben oder SNMP nicht konfiguriert haben eine 
+NullPointerException zurückgeben die noch nicht behandelt wird aber das Programm nicht beendet. 
--- src/it/bx/fallmerayer/tfo/snmp/Test.java
+++ src/it/bx/fallmerayer/tfo/snmp/Test.java
@@ -1,43 +1,89 @@
 package it.bx.fallmerayer.tfo.snmp;
 
 import org.snmp4j.CommunityTarget;
-
 import org.snmp4j.PDU;
-
 import org.snmp4j.Snmp;
-
 import org.snmp4j.TransportMapping;
-
 import org.snmp4j.event.ResponseEvent;
-
-import org.snmp4j.event.ResponseListener;
-
 import org.snmp4j.mp.SnmpConstants;
-
 import org.snmp4j.smi.*;
-
 import org.snmp4j.transport.DefaultUdpTransportMapping;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Scanner;
 
 public class Test {
     public static final String READ_COMMUNITY = "public";
     public static final String WRITE_COMMUNITY= "private";
-    public static final int mSNMPVersion = 0;
-    public static final String OID_UPS_OUTLET_GROUP1 = "1.3.6.1.4.1.318.1.1.1.12.3.2.1.3.1";
     public static final String OID_SYS_NAME="1.3.6.1.2.1.1.5.0";
 
     public static void main(String[] args) {
-        try {
-            String strIPAddress = "127.0.0.1";
-            Test objSNMP = new Test();
-            int Value = 2;
-            String batteryCap =objSNMP.snmpGet(strIPAddress,READ_COMMUNITY,OID_SYS_NAME);
-        } catch (Exception e)
-        {
-            e.printStackTrace();
+        Test objSNMP = new Test();
+        int Value = 2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to my SNMP Tool \n functionalities: \n SNMPget \n getNetwork \n exit \n Enter Command: ");
+        String auswahl = sc.nextLine();
+        while(true) {
+            switch (auswahl){
+                case "SNMPget":
+                    System.out.println("Input ip address to perform snmp get:");
+                    Scanner scip = new Scanner(System.in);
+                    String strIPAddress = scip.nextLine();
+                    try {
+                        objSNMP.snmpGet(strIPAddress,READ_COMMUNITY,OID_SYS_NAME);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                break;
+
+                case "getNetwork":
+                    try {
+                        objSNMP.getNetwork();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                break;
+                case "exit":
+                    System.out.println("Quitting SNMP Tool, Bye");
+                    return;
+            }
+            System.out.println("functionalities: \n SNMPget \n getNetwork \n Enter Command: ");
+            auswahl = sc.nextLine();
         }
     }
 
-    public String snmpGet(String strAddress, String community, String strOID) {
+    public void getNetwork() throws IOException {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Please input your network-address (only /24 Networks are supported)");
+       String netzIp = sc.nextLine();
+       String newNetzIp = "";
+       int one, two, three, four;
+        System.out.println("Perfroming getNetwork...");
+       for (int i = 0; i <= 255; i++) {
+           String[] tempaddr = netzIp.split("\\.");
+           tempaddr[3] = Integer.toString(i);
+           one = Integer.parseInt(tempaddr[0]);
+           two = Integer.parseInt(tempaddr[1]);
+           three = Integer.parseInt(tempaddr[2]);
+           four = Integer.parseInt(tempaddr[3]);
+           InetAddress ias = InetAddress.getByAddress(new byte[] {
+                   (byte)one, (byte)two, (byte)three, (byte)four}
+           );
+           if(ias.isReachable(500)){
+               tempaddr[3] = Integer.toString(i);
+               newNetzIp = newNetzIp.concat(tempaddr[0]).concat(".").concat(tempaddr[1]).concat(".").concat(tempaddr[2]).concat(".").concat(tempaddr[3]);
+               System.out.println("Device found on: "+newNetzIp);
+               snmpGet(newNetzIp, READ_COMMUNITY, OID_SYS_NAME);
+           } else {
+               newNetzIp = newNetzIp.concat(tempaddr[0]).concat(".").concat(tempaddr[1]).concat(".").concat(tempaddr[2]).concat(".").concat(tempaddr[3]);
+               System.out.println("No device on ip address: "+newNetzIp);
+           }
+           newNetzIp = newNetzIp.concat(tempaddr[0]).concat(".").concat(tempaddr[1]).concat(".").concat(tempaddr[2]).concat(".").concat(tempaddr[3]);
+           newNetzIp = "";
+       }
+    }
+
+    public void snmpGet(String strAddress, String community, String strOID) {
         String str="";
 
         try {
@@ -75,6 +121,5 @@ public class Test {
             snmp.close();
         } catch(Exception e) { e.printStackTrace(); }
         System.out.println("Response="+str);
-        return str;
     }
 }
